115c2338d3b1af5728af497b5ea43934ca012a19
gyermich <yermichevagalina@gmail.com>
2020-12-05 23:22:08 -0800

    Break down into smaller components and style

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,10 @@
+import Directory from "../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+const HopePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HopePage;
